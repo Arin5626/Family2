043885b6da1a9098483f2ec889376acc0f6be01b
arind <arindubey5626@gmail.com>
2023-09-21 14:14:10 +0530

    new

--- app/src/main/java/testing45/myapplication/familu/MainActivity.java
+++ app/src/main/java/testing45/myapplication/familu/MainActivity.java
@@ -2,13 +2,28 @@ package testing45.myapplication.familu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 openActivity();
+            }
+        });
+    }
+
+    private void openActivity() {
+        Intent intent =new Intent(this,Dashboard.class);
+        startActivity(intent);
     }
 }
